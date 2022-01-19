7f03cd5cc36d9b4c540f379a16c6df58aa3a33d8
winrinseo <tjdnfls1234@naver.com>
2022-01-19 19:10:23 +0900

    선 긋기

---
+++ 2170.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <tuple>
+#define INF -1000000005
+using namespace std;
+using ll = long long;
+int N;
+pair<int,int> p[200005];
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>N;
+    for(int i = 0;i<N;i++){
+        cin>>p[i].first>>p[i].second;
+    }
+    //오른쪽 좌표를 기준으로 정렬
+    sort(p,p+N);
+    //선분 병합.
+    int l = INF,r = INF;
+    ll result = 0;
+    for(int i = 0;i<N;i++){
+        if(r < p[i].first){
+            result += (r - l);
+            l = p[i].first , r = p[i].second;
+        }else{
+            if(r < p[i].second)
+                r = p[i].second;
+        }
+    }
+    result += (r - l);
+    cout<<result;
+}
