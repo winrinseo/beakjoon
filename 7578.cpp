fb74dc0da779553b90be389584708e5921745e6e
winrinseo <tjdnfls1234@naver.com>
2022-01-28 17:02:31 +0900

    공장

--- 7578.cpp
+++ 7578.cpp
@@ -4,14 +4,17 @@
 using namespace std;
 int N;
 using ll = long long;
-pair<int,int> here[500005];
+ll T = 1;
+pair<ll,ll> here[500005];
 vector<ll> seg;
 ll arr[500005];
-ll upd(int idx,int st,int ed,int i){
-    if(i<st||ed<i) return seg[idx];
-    if(st==ed) return seg[idx]=1;
-    int mid = (st+ed)/2;
-    return seg[idx] = upd(idx*2,st,mid,i)+upd(idx*2+1,mid+1,ed,i);
+void upd(int i){
+     i += (T/2);
+     seg[i] = 1;
+     while(i > 1){
+        i/=2;
+        seg[i] = seg[i*2]+seg[i*2+1];
+    }
 }
 ll sum(int idx,int st,int ed,int l,int r){
     if(r<st||ed<l) return 0;
@@ -33,7 +36,8 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin>>N;
-    seg = vector<ll>(N*4+5,0);
+    while(T < (N*2)) T<<=1;
+    seg = vector<ll>(T,0);
     for(int i = 0;i<N;i++){
         cin>>arr[i];
     }
@@ -45,8 +49,9 @@ int main(){
     ll answer = 0;
     for(int i = 0;i<N;i++){
         int idx = binary(arr[i]);
-        answer += sum(1,0,N-1,idx,N-1);
-        upd(1,0,N-1,idx);
+        answer += sum(1,0,T/2-1,idx,T/2-1);
+        upd(idx);
     }
     cout<<answer;
+    
 }
