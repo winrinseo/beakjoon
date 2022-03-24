fdd09e2c4da27bbea7f926f5a2b5166cf08f6eb8
winrin <tjdnfls1234@naver.com>
2022-03-25 00:29:26 +0900

    현대모비스와 함께하는 부품관리

---
+++ 24724.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int T,N,A,B;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>T;
+    for(int t = 1;t<=T;t++){
+        cin>>N;
+        cin>>A>>B;
+        for(int i = 0;i<N;i++){
+            int a,b;cin>>a>>b;
+        }
+
+        cout<<"Material Management "<<t<<"\n";
+        cout<<"Classification ---- End!"<<"\n";
+    }
+    
+
+}
