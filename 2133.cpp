b1d6d243f051ad731350ee32b772d1fb666c1623
winrinseo <tjdnfls1234@naver.com>
2022-04-06 01:45:37 +0900

    타일 채우기

--- 2133.cpp
+++ 2133.cpp
@@ -6,9 +6,8 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin>>N;
-    dp[0] = 1 , dp[2] = 3,dp[4] = 11;
-    
-    for(int i = 6;i<=N;i++){
+    dp[0] = 1;
+    for(int i = 2;i<=N;i++){
         dp[i] = (3*dp[i-2]);
         //짝수 블록에선 무조건 두가지 방법씩 존재함
         for(int j = 4;j<=i;j+=2){
