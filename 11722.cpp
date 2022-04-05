8a33f77d32cff7417dce5f047abf8efb050c22b3
winrinseo <tjdnfls1234@naver.com>
2022-04-06 01:03:31 +0900

    가장 긴 감소하는 부분수열

---
+++ 11722.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int N;
+int p[1005],dp[1005];
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>N;
+    for(int i = 0;i<N;i++){
+        cin>>p[i];
+        dp[i] = 1;
+    }
+    int answer = 1;
+    for(int i = 1;i<N;i++){
+        for(int j = 0;j<i;j++){
+            if(p[j] > p[i]){
+                dp[i] = max(dp[i],dp[j]+1);
+                answer = max(answer,dp[i]);
+            }
+        }
+    }
+    cout<<answer;
+}
