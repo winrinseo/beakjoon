c5e4472a15fd189e9c8a86d618446d3d9babf775
winrinseo <tjdnfls1234@naver.com>
2022-04-08 23:49:15 +0900

    이동하기

---
+++ 11048.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int N,M;
+int board[1005][1005];
+int dp[1005][1005];
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>N>>M;
+    for(int i = 1;i<=N;i++){
+        for(int j = 1;j<=M;j++){
+            cin>>board[i][j];
+        }
+    }
+    for(int i = 1;i<=N;i++){
+        for(int j = 1;j<=M;j++){
+            dp[i][j] = max(dp[i][j],dp[i-1][j]+board[i][j]);
+            dp[i][j] = max(dp[i][j],dp[i-1][j-1]+board[i][j]);
+            dp[i][j] = max(dp[i][j],dp[i][j-1]+board[i][j]);
+        }
+    }
+    cout<<dp[N][M];
+}
