ad728555e764d03720d815ae1e0c08bd917d5e47
winrinseo <tjdnfls1234@naver.com>
2023-07-10 02:05:56 +0900

    제곱수 찾기

--- 10217.cpp
+++ 10217.cpp
@@ -9,6 +9,7 @@ struct edge{
 };
 struct Point{
     int cur,cost,dist;
+    
 };
 struct cmp{
     bool operator()(Point& a,Point& b){
@@ -19,13 +20,14 @@ int T,N,M,K;
 vector<edge> adj[105];
 int dist[105][10005];
 int dij(){
-    for(int i = 0;i<105;i++)
-        for(int j = 0;j<10005;j++)
+    for(int i = 0;i<=N;i++)
+        for(int j = 0;j<=M;j++)
             dist[i][j] = INF;
         
     priority_queue<Point,vector<Point>,cmp> pq;
     dist[1][0] = 0;
     pq.push({1,0,0});
+
     while(!pq.empty()){
         Point curr = pq.top();pq.pop();
         if(dist[curr.cur][curr.cost] < curr.dist) continue;
@@ -33,9 +35,11 @@ int dij(){
         for(auto& next : adj[curr.cur]){
             int ncost = curr.cost+next.c;
             int ndist = curr.dist+next.d;
-            if(ncost > 10000) continue;
+            if(ncost > M) continue;
+
             if(dist[next.v][ncost] > ndist){
                 dist[next.v][ncost] = ndist;
+                
                 pq.push({next.v,ncost,ndist});
             }
         }
@@ -49,10 +53,9 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin>>T;
-
     while(T--){
         cin>>N>>M>>K;
-        for(int i = 0;i<105;i++) adj[i].clear();
+        for(int i = 0;i<=N;i++) adj[i].clear();
         for(int i = 0;i<K;i++){
             int a,b,c,d;cin>>a>>b>>c>>d;
             adj[a].push_back({b,c,d});
---
+++ 1025.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int N,M;
+int board[20][20];
+
+int solve(int y , int x){
+    int ret = -1;
+    for(int i = 0;i<4;i++){
+
+        for(int i = 0;i<N;i++){
+            for(int j = 0 ;j<M;j++){
+                int xx = j , yy = i;
+                int r = 0;
+                while(( xx >= 0 && xx < M) && (yy >= 0 && yy < N)){
+                    r *= 10;
+                    r += board[yy][xx];
+                    
+                    if((int)sqrt(r) * (int)sqrt(r) == r){
+                        ret = max(ret , r);
+                    }
+                    xx += x;
+                    yy += y;
+
+                }
+            }
+        }
+        // cout<<y<<" "<<x<<"\n";
+        if( i % 2 == 0) y *= -1;
+        else x *= -1;
+    }
+    // cout<<ret<<"\n";
+    return ret;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin>>N>>M;
+
+    for(int i = 0;i<N;i++){
+        for(int j = 0;j<M;j++){
+            char a;cin>>a;
+            board[i][j] = (a - '0');
+            
+        }
+    }
+    int answer = -1;
+    for(int y = 0;y<=N;y++){
+        for(int x = 0; x<=M;x++){
+            if(y == 0 && x == 0) continue;
+            answer = max(answer , solve(y , x));
+        }
+    }
+    cout<<answer;
+}
