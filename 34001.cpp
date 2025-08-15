fa4847800e9f73e3a77e8cfd1c3c06f0f767f379
winrin <tjdnfls1234@naver.com>
2025-08-16 03:57:19 +0900

    임스의 일일 퀘스트

---
+++ 34001.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>N;
+
+
+    if(N >= 220){
+        cout<<100<<" ";
+    }else if(N >= 210){
+        cout<<300<<" ";
+    }else if(N >= 200){
+        cout<<500<<" ";
+    }else{
+        cout<<0<<" ";
+    }
+
+    if(N >= 225){
+        cout<<100<<" ";
+    }else if(N >= 220){
+        cout<<300<<" ";
+    }else if(N >= 210){
+        cout<<500<<" ";
+    }else{
+        cout<<0<<" ";
+    }
+
+    if(N >= 230){
+        cout<<100<<" ";
+    }else if(N >= 225){
+        cout<<300<<" ";
+    }else if(N >= 220){
+        cout<<500<<" ";
+    }else{
+        cout<<0<<" ";
+    }
+
+    if(N >= 235){
+        cout<<100<<" ";
+    }else if(N >= 230){
+        cout<<300<<" ";
+    }else if(N >= 225){
+        cout<<500<<" ";
+    }else{
+        cout<<0<<" ";
+    }
+
+    if(N >= 245){
+        cout<<100<<" ";
+    }else if(N >= 235){
+        cout<<300<<" ";
+    }else if(N >= 230){
+        cout<<500<<" ";
+    }else{
+        cout<<0<<" ";
+    }
+
+    if(N >= 250){
+        cout<<100<<" ";
+    }else if(N >= 245){
+        cout<<300<<" ";
+    }else if(N >= 235){
+        cout<<500<<" ";
+    }else{
+        cout<<0<<" ";
+    }
+    cout<<"\n";
+
+    if(N >= 270){
+        cout<<100<<" ";
+    }else if(N >= 265){
+        cout<<300<<" ";
+    }else if(N >= 260){
+        cout<<500<<" ";
+    }else{
+        cout<<0<<" ";
+    }
+
+    if(N >= 275){
+        cout<<100<<" ";
+    }else if(N >= 270){
+        cout<<300<<" ";
+    }else if(N >= 265){
+        cout<<500<<" ";
+    }else{
+        cout<<0<<" ";
+    }
+
+    if(N >= 280){
+        cout<<100<<" ";
+    }else if(N >= 275){
+        cout<<300<<" ";
+    }else if(N >= 270){
+        cout<<500<<" ";
+    }else{
+        cout<<0<<" ";
+    }
+
+    if(N >= 285){
+        cout<<100<<" ";
+    }else if(N >= 280){
+        cout<<300<<" ";
+    }else if(N >= 275){
+        cout<<500<<" ";
+    }else{
+        cout<<0<<" ";
+    }
+
+    if(N >= 290){
+        cout<<100<<" ";
+    }else if(N >= 285){
+        cout<<300<<" ";
+    }else if(N >= 280){
+        cout<<500<<" ";
+    }else{
+        cout<<0<<" ";
+    }
+
+    if(N >= 295){
+        cout<<100<<" ";
+    }else if(N >= 290){
+        cout<<300<<" ";
+    }else if(N >= 285){
+        cout<<500<<" ";
+    }else{
+        cout<<0<<" ";
+    }
+
+    if(N >= 300){
+        cout<<100<<" ";
+    }else if(N >= 295){
+        cout<<300<<" ";
+    }else if(N >= 290){
+        cout<<500<<" ";
+    }else{
+        cout<<0<<" ";
+    }
+}
