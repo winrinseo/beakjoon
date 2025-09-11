6225b49c2f8b69376cddae912d6b7abb99298320
winrin <tjdnfls1234@naver.com>
2025-09-12 00:57:37 +0900

    DORO

---
+++ 34073.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin>>N;
+    for(int i = 0;i<N;i++){
+        string a;cin>>a;
+        cout<<a<<"DORO"<<" ";
+    }
+}
