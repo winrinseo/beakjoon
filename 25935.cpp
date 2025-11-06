086044fc8c9b99bbe0a36336c1c7e45346fa0c9a
winrin <tjdnfls1234@naver.com>
2025-11-06 20:47:46 +0900

    Lemonade Stand

---
+++ 25935.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int d,x,s;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin>>N;
+
+
+    while(N--){
+        cin>>d>>x>>s;
+
+        int p_lemon = 505 , p_sugar = 505;
+        int remain_sugar = 0;
+        int answer = 0;
+        for(int i = 0;i<d;i++){
+            int c , pl , ps;
+            cin>>c>>pl>>ps;
+            
+            p_lemon = min(p_lemon , pl);
+            p_sugar = min(p_sugar , ps);
+
+            //레몬부터 계산
+            answer += (x * c * p_lemon);
+            cout<<answer<<" ";
+            // 설탕은 필요 온스부터 계산
+            int need_s = max(0 , s * c - remain_sugar);
+            // 구매해야 할 포대 수
+            int buy = need_s / 80;
+            if(need_s % 80 != 0) buy++;
+
+            answer += (buy * p_sugar);
+
+            remain_sugar = (buy * 80 + remain_sugar) - (s * c);
+
+            cout<<answer<<"\n";
+        }
+
+        cout<<answer<<"\n";
+    }
+}
